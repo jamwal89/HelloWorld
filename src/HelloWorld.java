4569f91e2be4f7bcf7034ff994d393db72b51fcd
NehaJamwal <nehajamwal89@gmail.com>
2016-09-02 11:47:08 -0400

    new commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,18 +3,18 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// This is my first Project
-		System.out.println("----------------------------------------");
-		System.out.println("|JAVA BANK ATM RECIEPT|");
-		System.out.println("|September 2nd 2016   |  ");
-		System.out.println("|ATM Location 123     |");
-		System.out.println("| Account Number 3216 |");
-		System.out.println("|Customer: John       |");	
-		System.out.println("|Transaction Type: dep|");
-		System.out.println("|Transaction Amt: 100 |");
-		System.out.println("|Account Bal: 200     |");
-		System.out.println("|Thankyou very much for Banking with us|");
-		System.out.println("|Have a nice day|");
-		System.out.println("----------------------------------------------");
+		System.out.println("+---------------------------------------+");
+		System.out.println("|JAVA BANK ATM RECIEPT                  |");
+		System.out.println("|September 2nd 2016                     |  ");
+		System.out.println("|ATM Location 123                       |");
+		System.out.println("| Account Number 3216                   |");
+		System.out.println("|Customer: John                         |");	
+		System.out.println("|Transaction Type: dep                  |");
+		System.out.println("|Transaction Amt: 100                   |");
+		System.out.println("|Account Bal: 200                       |");
+		System.out.println("|Thankyou very much for Banking with us |");
+		System.out.println("|Have a nice day                        |");
+		System.out.println("+---------------------------------------+");
 	}
 
 }
