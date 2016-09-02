49bf00f02b7e6edf4474f8d3be0fd29c4af3a1f3
NehaJamwal <nehajamwal89@gmail.com>
2016-09-02 11:26:33 -0400

    Added a new line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello World");
-System.out.println("NehaJamwal");
+		// This is my first Project
+		System.out.println("Hello World");
+		System.out.println("NehaJamwal");
+		Sytsem.out.println("Love Java");
 	}
 
 }
